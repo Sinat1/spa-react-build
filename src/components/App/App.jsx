f3fc959189f1744971b2d1b6ead0522677df5137
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2024-07-04 15:23:14 +0300

    add basic repository setup

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import { AppContainer } from './App.styled.js';
+
+export const App = () => {
+  return (
+    <AppContainer>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </AppContainer>
+  );
+};
+
+export default App;
